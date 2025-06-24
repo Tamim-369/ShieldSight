cb7f6fa7f1a2ed5f8345686bc8e373c9a3ecb7d8
Tamim-369 <ashiqurrahmantamim369@gmail.com>
2025-06-24 09:15:26 +0600

    trying to fix auto run issues

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,2 @@
-from .utils import setup_auto_start, check_existing_process
+from .utils import setup_auto_start, check_existing_process, create_lock_file, remove_lock_file
 from .config import CONFIG,get_close_tab_action, set_close_tab_action
